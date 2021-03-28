04d1171910d75d9630b2786636e004f1558faeca
Eduardo Tai <eduardotaidsr@gmail.com>
2021-03-27 21:27:40 -0300

    todas requisições do app feitas

---
+++ fundamentos-nodejs/src/index.js
@@ -0,0 +1,143 @@
+const express = require ("express")
+const { v4: uuidv4 } = require ("uuid")
+
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+function VerifyIfExistsAccountCpf (request, response, next) {
+    const { cpf } = request.headers
+
+    const customer = customers.find((customer) => customer.cpf === cpf)
+
+       
+    if(!customer){
+        return response.status(400).json({error: "tem ninguem aqui com esse nome n"})
+    }
+
+    request.customer = customer
+
+    return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount
+        }else{
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body
+
+    const CustomerAlreadyExists = customers.some((customers) => customers.cpf === cpf)
+
+    if(CustomerAlreadyExists){
+        return response.status(400).json({error:     "Customer already exists"})
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.get("/statement/", VerifyIfExistsAccountCpf, (request, response) => {
+
+    const { customer } = request
+
+    return response.json(customer.statement)
+})
+
+app.post("/deposit",VerifyIfExistsAccountCpf, (request, response) => {
+    const { description, amount} = request.body
+
+    const { customer } = request
+
+    const statementOperation = { 
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post("/withdraw",VerifyIfExistsAccountCpf, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+    const balance = getBalance(customer.statement)
+
+    if(balance < amount){
+        response.status(400).json({error: "saldo insuficiente otario"})
+    }
+
+    const statementOperation = { 
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.get("/statement/date", VerifyIfExistsAccountCpf, (request, response) => {
+
+    const { customer } = request
+    const { date } = request.query
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return response.json(customer.statement)
+})
+
+app.put("/account",VerifyIfExistsAccountCpf, (request, response) => {
+    const {name} = request.body
+    const { customer } = request
+
+    customer.name = name
+
+
+
+    return response.status(201).send()
+})
+
+app.get("/account",VerifyIfExistsAccountCpf, (request, response) => {
+    const {customer} = request
+
+    return response.json(customer)
+})
+
+app.delete("/account",VerifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request
+
+    customers.splice(customer, 1)
+
+    return response.status(200).json(customers)
+})
+
+app.get("/balance",VerifyIfExistsAccountCpf,(request, response) => {
+    const { customer } = request
+    const balance = getBalance(customer.statement)
+
+    return response.json(balance)
+})
+app.listen(3333)
